b446e3e035db75e6b3a4e5d7ec526c18edb25e1a
marcelostsh <marcelostsh@gmail.com>
2025-04-19 10:56:39 -0300

    projeto vue3
    projeto vue3

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// Componente simplificado Olá Mundo
+</script>
+
+<template>
+  <div class="container">
+    <h1>Olá Mundo!</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 3rem;
+  color: #42b883;
+}
+</style>
